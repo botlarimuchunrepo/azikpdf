7af29095dc02dc03c4572cf2fee9447da38d5d7d
Abduazim <100690130+botlarimuchunrepo@users.noreply.github.com>
2022-04-18 23:29:10 +0500

    Create pdf.py

---
+++ pdf.py
@@ -0,0 +1,35 @@
+# !/USR/BIN/PYTHON
+# -*- COADING: UTF-8 -*-
+# COPYRIGHT ©️ 2021 NABILANAVAB
+
+'''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         [Nabil A Navab] 
+                         Email: nabilanavab@gmail.com
+                         Telegram: @nabilanavab
+ '''
+
+import logging
+from pyromod import listen
+from Configs.dm import Config
+from pyrogram import Client, idle
+
+# LOGGING INFO: DEBUG
+logging.basicConfig(
+    level = logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+# GLOBAL VARIABLES
+PDF = {}            # save images for generating pdf
+PROCESS = []        # to check current process
+invite_link = None
+
+# PLUGIN DIRECTORY
+plugin = dict(
+    root = "plugins"
+)
